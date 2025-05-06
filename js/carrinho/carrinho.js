9ac1d6c627628de18511fea8d7484bd051924c16
Cla <157390340+ClaMG@users.noreply.github.com>
2025-05-06 10:25:41 -0300

    linkagem

---
+++ js/carrinho/carrinho.js
@@ -0,0 +1,24 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const botoesOuvir = document.querySelectorAll('.audio_but');
+    const botoesVoltar = document.querySelectorAll('.butao.voltar');
+
+    botoesOuvir.forEach(botao => {
+    botao.addEventListener('click', () => {
+        const produto = botao.closest('.produto');
+        const telaAudio = produto.querySelector('.tela_audio');
+        if (telaAudio) {
+        telaAudio.style.display = 'flex';
+        }
+    });
+    });
+
+    botoesVoltar.forEach(botao => {
+    botao.addEventListener('click', () => {
+        const telaAudio = botao.closest('.tela_audio');
+        if (telaAudio) {
+        telaAudio.style.display = 'none';
+        }
+    });
+    });
+});
